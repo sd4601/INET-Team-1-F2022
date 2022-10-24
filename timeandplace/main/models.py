26cf2844d519b6bd0676beb5cf1b268be401af2f
Kaining Mao <chrissmao99@gmail.com>
2022-10-24 12:55:47 -0400

    create user,authenticate, bio models

--- timeandplace/main/models.py
+++ timeandplace/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class place(models.Model):
+    placename = models.CharField(max_length=100)
+    address = models.CharField(max_length=100) 
+    placetype  = model.CharField(max_length=20)
+    rating = models.IntegerField(default=0)
+    zipcode = models.CharField(max_length=6)
--- timeandplace/users/models.py
+++ timeandplace/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class user(models.Model):
+    username = models.CharField(max_length=150)
+    name = models.CharField(max_length=40)
+    def __str__(self):
+        return self.username
+
+class authenticate(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(max_length=40)
+    password = models.CharField(min_length=8)
+
+class bio(models.Model):
+    username = models.CharField(max_length=150)
+    age = models.IntegerField(default=0)
+    description = models.TextField(max_length=200)
+    occupation = models.BooleanField()
+
